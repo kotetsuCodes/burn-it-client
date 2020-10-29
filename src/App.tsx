65b4ecc005df3bf5b8a84beb11e7c8338499f6ad
Carl Brown <kotetsuCodes@gmail.com>
2020-10-28 20:30:32 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RootContainer from './containers/RootContainer'
+
+function App() {
+  return (
+      <RootContainer />
+  );
+}
+
+export default App;
---
+++ src/containers/RootContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Login from '../components/Login'
+
+function RootContainer() {
+  return (    
+      <Login />
+    
+  )
+}
+
+export default RootContainer
